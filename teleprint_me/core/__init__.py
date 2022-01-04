d940765193472716fd4599c98cf7144fc9e8a83f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2022-01-04 12:13:54 -0500

    Refactor core
    - Use core/__init__.py as utils
        - Instantiate generate and scrypt as utils
    - Remove database headers to avoid recursive imports

--- teleprint_me/core/__init__.py
+++ teleprint_me/core/__init__.py
@@ -1,17 +1,39 @@
+from datetime import datetime
+from dateutil import parser
+
+from teleprint_me.core.database import settings_path
 from teleprint_me.core.security import Generate
 from teleprint_me.core.security import Scrypt
 
-from teleprint_me.core.database import instance_path
-from teleprint_me.core.database import database_name
-from teleprint_me.core.database import database_path
-from teleprint_me.core.database import pragmas
-
-from teleprint_me.core.database import database
-from teleprint_me.core.database import User
-from teleprint_me.core.database import Interface
-from teleprint_me.core.database import Strategy
-from teleprint_me.core.database import Dataset
-from teleprint_me.core.database import init_database
+import json
 
 generate = Generate()
 scrypt = Scrypt()
+
+
+#
+# Settings
+#
+def get_settings() -> dict:
+    with open(settings_path, 'r') as file:
+        return json.load(file)
+
+
+def set_settings(data: dict):
+    with open(settings_path, 'w') as file:
+        file.write(json.dump(data))
+
+
+#
+# Datetime
+#
+def iso_to_datetime(data: list[dict]) -> list[dict]:
+    for item in data:
+        item['timestamp']: datetime = parser.parse(item['timestamp'])
+    return data
+
+
+def datetime_to_iso(data: list[dict]) -> list[dict]:
+    for item in data:
+        item['timestamp']: str = item['timestamp'].isoformat()
+    return data
