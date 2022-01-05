32e0c41c8ec798bc345cb705e79675520dabea42
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2022-01-05 18:51:33 -0500

    apply black format and update core imports

--- teleprint_me/__init__.py
+++ teleprint_me/__init__.py
@@ -1,64 +1,48 @@
-from datetime import datetime
+from os import environ, makedirs
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
-from os import environ
-from os import makedirs
-
-from teleprint_me.core import generate
-from teleprint_me.core import instance_path
-from teleprint_me.core import database_path
-from teleprint_me.core import database
-from teleprint_me.core import init_database
-
-from teleprint_me.blueprints import auth
-from teleprint_me.blueprints import interface
-from teleprint_me.blueprints import strategy
-from teleprint_me.blueprints import setting
-from teleprint_me.blueprints import client
-from teleprint_me.blueprints import portfolio
+from teleprint_me.blueprints import (
+    auth,
+    client,
+    interface,
+    portfolio,
+    setting,
+    strategy,
+)
+from teleprint_me.core import generate, sqlite, timestamp
 
 
 def create_app() -> Flask:
-    secret_key = environ.get('SECRET_KEY', generate.bytes())
+    secret_key = environ.get("SECRET_KEY", generate.bytes())
 
     app = Flask(
-        __name__,
-        instance_path=instance_path,
-        instance_relative_config=True
+        __name__, instance_path=sqlite.instance_path, instance_relative_config=True
     )
 
-    app.config.from_mapping(SECRET_KEY=secret_key, DATABASE=database_path)
+    app.config.from_mapping(SECRET_KEY=secret_key, DATABASE=sqlite.database_path)
 
     # Database
     try:
-        makedirs(instance_path)
+        makedirs(sqlite.instance_path)
     except (OSError,):
         pass
 
-    init_database(database)
+    sqlite.initialize()
 
     @app.before_request
     def _db_connect():
-        database.connect()
+        sqlite.database.connect()
 
     @app.teardown_request
     def _db_close(exc):
-        if not database.is_closed():
-            database.close()
+        if not sqlite.database.is_closed():
+            sqlite.database.close()
 
     # Blueprints
     @app.context_processor
     def utility_processor() -> dict:
-        def timestamp() -> str:
-            return datetime.now().isoformat()
-
-        utils = {
-            'zip': zip,
-            'list': list,
-            'timestamp': timestamp
-        }
+        utils = {"zip": zip, "list": list, "sqlite": sqlite, "timestamp": timestamp}
 
         return utils
 
@@ -74,18 +58,18 @@ def create_app() -> Flask:
     for bp in blueprints:
         app.register_blueprint(bp)
 
-    @app.route('/menu')
+    @app.route("/menu")
     @auth.required
     def menu():
-        return render_template('menu.html')
+        return render_template("menu.html")
 
-    @app.route('/license')
+    @app.route("/license")
     @auth.required
     def license():
-        return render_template('license.html')
+        return render_template("license.html")
 
-    app.add_url_rule('/', endpoint='index')
-    app.add_url_rule('/menu', endpoint='menu')
-    app.add_url_rule('/license', endpoint='license')
+    app.add_url_rule("/", endpoint="index")
+    app.add_url_rule("/menu", endpoint="menu")
+    app.add_url_rule("/license", endpoint="license")
 
     return app
