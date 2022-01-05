f9a5190b551f3a799a167b8b8c57037af1d81d7c
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2022-01-05 18:41:57 -0500

    use init for static class instances

--- teleprint_me/core/__init__.py
+++ teleprint_me/core/__init__.py
@@ -1,39 +1,4 @@
-from datetime import datetime
-from dateutil import parser
-
-from teleprint_me.core.database import settings_path
-from teleprint_me.core.security import Generate
-from teleprint_me.core.security import Scrypt
-
-import json
+from teleprint_me.core.security import Generate, Scrypt
 
 generate = Generate()
 scrypt = Scrypt()
-
-
-#
-# Settings
-#
-def get_settings() -> dict:
-    with open(settings_path, 'r') as file:
-        return json.load(file)
-
-
-def set_settings(data: dict):
-    with open(settings_path, 'w') as file:
-        file.write(json.dump(data))
-
-
-#
-# Datetime
-#
-def iso_to_datetime(data: list[dict]) -> list[dict]:
-    for item in data:
-        item['timestamp']: datetime = parser.parse(item['timestamp'])
-    return data
-
-
-def datetime_to_iso(data: list[dict]) -> list[dict]:
-    for item in data:
-        item['timestamp']: str = item['timestamp'].isoformat()
-    return data
