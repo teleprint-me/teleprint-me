239b445d3ef10c7285064a612fd7f36ba8581dbf
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2022-11-21 21:04:28 -0500

    Refactor static and modules paths to apply DRY
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- main.py
+++ main.py
@@ -8,48 +8,38 @@ app = Bottle()
 #
 # Static Content
 #
-@app.route("/images/<filepath:path>")
-def images(filepath):
-    return static_file(filepath, root=static_path.images)
+@app.route("/static/<filepath:path>")
+def static(filepath):
+    return static_file(filepath, root=static_path.directory)
 
 
-@app.route("/styles/<filepath:path>")
-def styles(filepath):
-    return static_file(filepath, root=static_path.styles)
-
-
-@app.route("/scripts/<filepath:path>")
-def scripts(filepath):
-    return static_file(filepath, root=static_path.scripts)
-
-
-@app.route("/grassroots/<filepath:path>")
-def grassroots(filepath):
-    return static_file(filepath, root=module_path.grassroots)
+@app.route("/modules/<filepath:path>")
+def modules(filepath):
+    return static_file(filepath, root=module_path.directory)
 
 
 #
 # Dynamic Content
 #
 @app.route("/")
-def root():
-    return static_file("index.html", root=static_path.views)
+def index():
+    return static_file("index.html", root=static_path.get("views"))
 
 
 @app.route("/profile")
 def view_profile():
-    return static_file("profile.html", root=static_path.views)
+    return static_file("profile.html", root=static_path.get("views"))
 
 
 @app.route("/portfolio")
 def view_portfolio():
-    return static_file("portfolio.html", root=static_path.views)
+    return static_file("portfolio.html", root=static_path.get("views"))
 
 
 @app.route("/contact")
 def view_contact():
-    return static_file("contact.html", root=static_path.views)
+    return static_file("contact.html", root=static_path.get("views"))
 
 
-# these settings are not to be used in production
+# development settings are not to be used in production
 run(app, host="localhost", port=8080, reloader=True, debug=True)
--- teleprint_me/static.py
+++ teleprint_me/static.py
@@ -1,8 +1,9 @@
 from os import environ
 
 
-class EnvironPath(object):
-    def __init__(self):
+class StaticPath:
+    def __init__(self, root: str = ""):
+        self.__root: str = root
         self.__cwd: str = environ.get("PWD", ".")
 
     @property
@@ -13,17 +14,6 @@ class EnvironPath(object):
     def root(self) -> str:
         return self.__root
 
-    @root.setter
-    def root(self, name: str):
-        self.__root = name
-
-
-class StaticPath(EnvironPath):
-    def __init__(self, root: str = ""):
-        super(StaticPath, self).__init__()
-
-        self.root: str = root
-
     @property
     def directory(self) -> str:
         if self.root:
@@ -33,45 +23,16 @@ class StaticPath(EnvironPath):
     def get(self, name: str) -> str:
         return f"{self.directory}/{name}"
 
-    @property
-    def views(self) -> str:
-        return self.get("views")
 
-    @property
-    def templates(self) -> str:
-        return self.get("templates")
-
-    @property
-    def styles(self) -> str:
-        return self.get("styles")
-
-    @property
-    def scripts(self) -> str:
-        return self.get("scripts")
-
-    @property
-    def images(self) -> str:
-        return self.get("images")
-
-
-class ModulePath(EnvironPath):
+class ModulePath(StaticPath):
     def __init__(self, root: str = ""):
-        super(ModulePath, self).__init__()
-
-        self.root: str = root
+        super(ModulePath, self).__init__(root=root)
 
     @property
     def directory(self) -> str:
         if self.root:
             return f"{self.cwd}/{self.root}"
         return f"{self.cwd}/modules"
-
-    def get(self, name: str) -> str:
-        return f"{self.directory}/{name}"
-
-    @property
-    def grassroots(self) -> str:
-        return self.get("grassroots")
 
 
 static_path: StaticPath = StaticPath()
