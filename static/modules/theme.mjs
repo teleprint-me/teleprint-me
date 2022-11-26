a503c3791820194dc8b52aa820b6f438d39527a1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2022-11-26 15:45:16 -0500

    Use a callback instead of a bound method to avoid undefined references
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- static/modules/theme.mjs
+++ static/modules/theme.mjs
@@ -1,38 +1,51 @@
 export class Theme {
-    light(a) {
-        const i = a.querySelector('i');
-        const span = a.querySelector('span');
-
-        a.setAttribute('data-theme', 'dark');
-        a.classList.replace('theme-moon', 'theme-sun');
-        i.classList.replace('bxs-moon', 'bxs-sun');
-        span.innerText = 'Light';
+    get attribute() {
+        return 'data-theme';
     }
 
-    dark(a) {
-        const i = a.querySelector('i');
-        const span = a.querySelector('span');
+    light() {
+        const attribute = this.attribute;
 
-        a.setAttribute('data-theme', 'light');
-        a.classList.replace('theme-sun', 'theme-moon');
-        i.classList.replace('bxs-sun', 'bxs-moon');
-        span.innerText = 'Dark';
+        return function (a) {
+            const i = a.querySelector('i');
+            const span = a.querySelector('span');
+
+            a.setAttribute(attribute, 'dark');
+            a.classList.replace('theme-moon', 'theme-sun');
+            i.classList.replace('bxs-moon', 'bxs-sun');
+            span.innerText = 'Light';
+        };
+    }
+
+    dark() {
+        const attribute = this.attribute;
+
+        return function (a) {
+            const i = a.querySelector('i');
+            const span = a.querySelector('span');
+
+            a.setAttribute(attribute, 'light');
+            a.classList.replace('theme-sun', 'theme-moon');
+            i.classList.replace('bxs-sun', 'bxs-moon');
+            span.innerText = 'Dark';
+        };
     }
 
     click() {
-        const light = this.light;
-        const dark = this.dark;
+        const attribute = this.attribute;
+        const light = this.light();
+        const dark = this.dark();
 
         return function (event) {
             const a = event.target.closest('a');
 
-            if ('light' === a.getAttribute('data-theme')) {
+            if ('light' === a.getAttribute(attribute)) {
                 light(a);
             } else {
                 dark(a);
             }
 
-            localStorage.setItem('data-theme', a.getAttribute('data-theme'));
+            localStorage.setItem(attribute, a.getAttribute(attribute));
         };
     }
 
