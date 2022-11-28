30a9a1dfc930e49728d2da268e2ffcad567bbf2d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2022-11-28 12:04:09 -0500

    Refactor app module to apply seperation of concerns
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- static/modules/app.mjs
+++ static/modules/app.mjs
@@ -2,22 +2,34 @@ import { AsyncRequest } from './async-request.mjs';
 import { HashRouter } from './hash-router.mjs';
 import { Theme } from './theme.mjs';
 
-const request = new AsyncRequest();
+const setup_router = async () => {
+    const request = new AsyncRequest();
 
-const t_router = await request.template('/static/views/router.html');
+    const template = await request.template('/static/templates/router.html');
 
-const header = document.querySelector('#router');
-header.appendChild(t_router.content.cloneNode(true));
+    const header = document.querySelector('#router');
+    header.appendChild(template.content.cloneNode(true));
 
-const routes = {
-    404: '/static/views/404.html',
-    '#/': '/static/views/profile.html',
-    '#/portfolio': '/static/views/portfolio.html',
-    '#/contact': '/static/views/contact.html'
+    const routes = {
+        404: '/static/views/404.html',
+        '#/': '/static/views/profile.html',
+        '#/portfolio': '/static/views/portfolio.html',
+        '#/contact': '/static/views/contact.html'
+    };
+
+    const router = new HashRouter('#app', routes);
+    router.init();
 };
 
-const router = new HashRouter('#app', routes);
-const theme = new Theme();
+const setup_theme = () => {
+    const theme = new Theme();
 
-router.init();
-theme.init();
+    theme.init();
+};
+
+const main = async () => {
+    await setup_router();
+    setup_theme();
+};
+
+main();
