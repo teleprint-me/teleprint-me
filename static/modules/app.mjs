e7457120ed2c7568cbb52c6b907ce45cc6d90dd2
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2022-12-13 18:55:50 -0500

    Add app-main component
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- static/modules/app.mjs
+++ static/modules/app.mjs
@@ -1,19 +1,30 @@
 import { Register } from './register.mjs';
 import { HashRouter } from './hash-router.mjs';
 import { Theme } from './theme.mjs';
-import { AppRouter } from '../components/router.mjs';
+import { AppRouter } from '../components/router/router.mjs';
+import { AppMain } from '../components/main/main.mjs';
 
 const components = [
     {
         name: 'app-router',
         constructor: AppRouter
+    },
+    {
+        name: 'app-main',
+        constructor: AppMain
     }
 ];
 
 const register = new Register();
+
 console.log('Has Template Support:', register.hasTemplateSupport);
 console.log('Has Shadow Support:', register.hasShadowSupport);
+
 register.components(...components);
+
+for (let component of components) {
+    console.log('registered', component);
+}
 
 const setup_router = async () => {
     const routes = {
@@ -23,7 +34,7 @@ const setup_router = async () => {
         '#!/contact': '/static/views/contact.html'
     };
 
-    const router = new HashRouter('#app', routes);
+    const router = new HashRouter('app-main', routes);
 
     router.init('app-router');
 };
