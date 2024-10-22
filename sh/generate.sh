75964b59049b7787217ac3c846d15f5e73f3b899
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-21 21:51:33 -0400

    sh(feat): Add initial generation script for creating static pages from text

---
+++ sh/generate.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Configuration variables
+config=".prettierrc"
+template="template.html"
+filename="Teach-Yourself-Programming-in-Ten-Years"
+markdown="${filename}.md"
+html="${filename}.html"
+
+# Remove old HTML file, if it exists
+rm -f "$html"
+
+# Convert Markdown to HTML with Pandoc, disabling smart quotes
+pandoc -f markdown-smart -t html "$markdown" -o "$html" \
+    --template="$template" \
+    --css=style.css \
+    --metadata pagetitle="Teach Yourself Programming in Ten Years"
+
+# Format HTML using Prettier with specified config
+prettier --ignore-path --config "$config" --write "$html"
