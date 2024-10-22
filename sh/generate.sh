9f53ee5fe9e1677287632d7ba494cfda7890b048
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-22 03:49:49 -0400

    sh(generate): Add generation rules for static html views

--- sh/generate.sh
+++ sh/generate.sh
@@ -2,21 +2,50 @@
 
 # Copyright (C) 2020 Austin Berrio
 
-# Configuration variables
+echo "generating static content..."
+
+# Paths
 config=".prettierrc"
-template="template.html"
-filename="Teach-Yourself-Programming-in-Ten-Years"
-markdown="${filename}.md"
-html="${filename}.html"
+template_main="static/templates/main.html"
+template_mathjax="static/templates/mathjax.html"
+css="static/styles/style.css"
 
-# Remove old HTML file, if it exists
-rm -f "$html"
+# Generate blog articles
+mkdir -p static/views/blog
+for file in docs/blog/*.md; do
+    filename=$(basename "$file" .md)
+    echo "generating ${filename}"
+    pandoc -f markdown-smart -t html "$file" \
+        --template="$template_main" --css="$css" \
+        -o "static/views/blog/$filename.html"
+    prettier --ignore-path --config "$config" \
+        --write "static/views/blog/$filename.html"
+done
 
-# Convert Markdown to HTML with Pandoc, disabling smart quotes
-pandoc -f markdown-smart -t html "$markdown" -o "$html" \
-    --template="$template" \
-    --css=style.css \
-    --metadata pagetitle="Teach Yourself Programming in Ten Years"
+# Generate technical articles
+mkdir -p static/views/technical
+for file in docs/technical/*.md; do
+    filename=$(basename "$file" .md)
+    echo "generating ${filename}"
+    pandoc -f markdown+tex_math_dollars -t html "$file" \
+        --template="$template_mathjax" --css="$css" \
+        -o "static/views/technical/$filename.html"
+    prettier --ignore-path --config "$config" \
+        --write "static/views/technical/$filename.html"
+done
 
-# Format HTML using Prettier with specified config
-prettier --ignore-path --config "$config" --write "$html"
+# Generate core views (like index.md)
+for file in docs/views/*.md; do
+    filename=$(basename "$file" .md)
+    echo "generating ${filename}"
+    pandoc -f markdown-smart -t html "$file" \
+        --template="$template_main" --css="$css" \
+        -o "static/views/$filename.html"
+    prettier --ignore-path --config "$config" \
+        --write "static/views/$filename.html"
+done
+
+# Copy index.html to the root if needed
+cp static/views/index.html ./index.html
+
+echo "done!"
